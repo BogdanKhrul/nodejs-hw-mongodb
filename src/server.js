0b19e0e7c0455116ded3d2b995b87dc717332966
BogdanKhrul <bobb52654@gmail.com>
2025-02-06 17:14:56 +0100

    F

---
+++ src/server.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getContacts, getContactById } from './services/contacts.js';
+import { getEnvVar } from './utils/env.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res, next) => {
+    try {
+      const contacts = await getContacts();
+
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Something went wrong',
+        error: error.message,
+      });
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        res.status(404).json({
+          message: 'Contact not found',
+        });
+        return;
+      }
+
+      res.status(200).json({
+        status: 200,
+        message: `Succesfully found contact with id ${contactId}`,
+        data: contact,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Something went wrong',
+        error: error.message,
+      });
+    }
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
