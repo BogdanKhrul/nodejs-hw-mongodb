b08a5fe0c76c4644b76658e301da58a2f02b5a15
BogdanKhrul <bobb52654@gmail.com>
2025-02-26 16:30:57 +0100

    hw3-test1

---
+++ src/routers/contacts.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper';
+import {
+  getContactsController,
+  getContactsByIdController,
+  createContactController,
+  deleteContactController,
+  upsertContactController,
+  patchContactController,
+} from '../controllers/contacts.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getContactsController));
+router.get('/contacts/:contactsId', ctrlWrapper(getContactsByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
+router.delete('/contacts/:contactsId', ctrlWrapper(deleteContactController));
+router.put('/contacts/:contactsId', ctrlWrapper(upsertContactController));
+router.patch('/contacts/:contactsId', ctrlWrapper(patchContactController));
+
+export default router;
