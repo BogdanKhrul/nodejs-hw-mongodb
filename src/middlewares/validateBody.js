de3c3a10fa0831f637b6d037e7238bc621e43d0d
BogdanKhrul <bobb52654@gmail.com>
2025-02-27 16:00:50 +0100

    hw4-test1

---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,22 @@
+import createHttpError from 'http-errors';
+
+export const validateBody = (schema) => async (req, res, next) => {
+  try {
+    await schema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+    next();
+  } catch (err) {
+    const error = createHttpError(400, 'BadRequestError', {
+      data: {
+        message: 'Bad request',
+        errors: err.details.map((detail) => detail.message),
+      },
+    });
+    res.status(400).json({
+      status: error.status,
+      message: error.message,
+      data: error.data,
+    });
+  }
+};
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,42 @@
 import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+export const getContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsCollection.find();
+
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
+  }
+
+  if (filter.isFavourite !== undefined) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (id) => {
@@ -14,7 +48,6 @@ export const createContact = async (contact) => {
   const contacts = await ContactsCollection.create(contact);
   return contacts;
 };
-
 
 export const updateContact = async (contactId, payload, options = {}) => {
   const rawResult = await ContactsCollection.findOneAndUpdate(
@@ -35,10 +68,10 @@ export const updateContact = async (contactId, payload, options = {}) => {
   };
 };
 
-  export const deleteContact = async (contactId) => {
-    const contact = await ContactsCollection.findByIdAndDelete({
-      _id: contactId,
-    });
-  
-    return contact;
-  };
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findByIdAndDelete({
+    _id: contactId,
+  });
+
+  return contact;
+};
