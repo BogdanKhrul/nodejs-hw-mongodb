c99c7f0342989aa934696304cd0416e36466819e
BogdanKhrul <bobb52654@gmail.com>
2025-03-01 10:40:53 +0100

    hw4-fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -8,18 +8,18 @@ export const createContactSchema = Joi.object({
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
-// export const updateContactSchema = Joi.object({
-//   name: Joi.string().min(3).max(20),
-//   phoneNumber: Joi.string().email().min(3).max(20),
-//   email: Joi.string().email().min(3).max(20),
-//   isFavourite: Joi.boolean(),
-//   contactType: Joi.string().valid('work', 'home', 'personal'),
-// });
-
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().email().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
-}).min(1);
+});
+
+// export const updateContactSchema = Joi.object({
+//   name: Joi.string().min(3).max(20),
+//   phoneNumber: Joi.string().min(3).max(20),
+//   email: Joi.string().email().min(3).max(20),
+//   isFavourite: Joi.boolean(),
+//   contactType: Joi.string().valid('work', 'home', 'personal'),
+// }).min(1);
