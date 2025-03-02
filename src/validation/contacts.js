6ae79898fda6815cff0902b222940755d3304094
BogdanKhrul <bobb52654@gmail.com>
2025-03-02 16:35:10 +0100

    hw4-fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -10,7 +10,7 @@ export const createContactSchema = Joi.object({
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().email().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
